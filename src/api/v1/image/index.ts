69972a3c50f9303b154e097d023679869a9650a1
nidble <abertulla@gmail.com>
2021-06-18 15:49:51 +0200

    refactoring Image API

--- src/api/v1/image/index.ts
+++ src/api/v1/image/index.ts
@@ -9,31 +9,27 @@ const response = (res: Response, payload: ResponsePayload, httpStatus = 200, hea
   send(res, httpStatus, payload, headers)
 }
 
-/*
-fieldname: 'image', originalname: '32178.jpg', mimetype: 'image/jpeg', destination: 'uploads/',
-filename: '48fafda...743', path: 'uploads/48fafda...743', size: 683305
-*/
-const ALLOWED_MIMETYPES = ['image/jpeg']
-export const post = (queue: { enqueue: Dispatch }) => async (req: Request, res: Response) => {
-  try {
-    const { username = '', latitude, longitude } = req.body ?? {}
-    if ('' === username) {
-      const error = { message: 'username field is mandatory', field: 'username' }
-      return response(res, { type: 'Error', errors: [error] }, 422)
+const extractJob = (req: Request): ResponsePayload => {
+  const { username = '', latitude, longitude } = req.body ?? {}
+  if ('' === username) {
+    const error = { message: 'username field is mandatory', field: 'username' }
+    return { type: 'Error', errors: [error] }
+  }
+  const { fieldname, originalname, mimetype, size: weight, path } = req.file ?? {}
+  if (!fieldname || !originalname || !weight || !path) {
+    const error = { message: 'uploaded image is invalid or broken' }
+    return { type: 'Error', errors: [error] }
+  }
+  if (!mimetype || !ALLOWED_MIMETYPES.includes(mimetype)) {
+    const error = {
+      message: `mimetype not supported, accepted only: ${ALLOWED_MIMETYPES.join(';')}`,
     }
-    const { fieldname, originalname, mimetype, size: weight, path } = req.file ?? {}
-    if (!fieldname || !originalname || !weight || !path) {
-      const error = { message: 'uploaded image is invalid or broken' }
-      return response(res, { type: 'Error', errors: [error] }, 422)
-    }
-    if (!mimetype || !ALLOWED_MIMETYPES.includes(mimetype)) {
-      const error = {
-        message: `mimetype not supported, accepted only: ${ALLOWED_MIMETYPES.join(';')}`,
-      }
-      return response(res, { type: 'Error', errors: [error] }, 422)
-    }
-    // TODO: size/weight validator
-    const payload = {
+    return { type: 'Error', errors: [error] }
+  }
+  // TODO: size/weight validator
+  return {
+    type: 'Success',
+    data: {
       fieldname,
       originalname,
       mimetype,
@@ -44,10 +40,25 @@ export const post = (queue: { enqueue: Dispatch }) => async (req: Request, res: 
       latitude: Number(latitude) || null,
       timestamp: time(),
       status: 'ACCEPTED',
+    },
+  }
+}
+
+/*
+fieldname: 'image', originalname: '32178.jpg', mimetype: 'image/jpeg', destination: 'uploads/',
+filename: '48fafda...743', path: 'uploads/48fafda...743', size: 683305
+*/
+const ALLOWED_MIMETYPES = ['image/jpeg']
+export const post = (queue: { enqueue: Dispatch }) => async (req: Request, res: Response) => {
+  try {
+    const job = extractJob(req)
+    if ('Error' === job.type) {
+      return response(res, job, 422)
     }
-    await queue.enqueue(payload)
+
+    await queue.enqueue(job.data)
     // accepted
-    response(res, { type: 'Success', data: payload }, 202)
+    response(res, job, 202)
   } catch (e) {
     logger.error(e)
     // TODO: maybe choose different error verbosity between production and dev (generic vs detailed)

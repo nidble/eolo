ca6c4407799140881dba603cf039e6d2ed1bdadd
Antonino Bertulla <abertulla@gmail.com>
2021-10-21 10:13:47 +0200

    Skip useless eslint rule for utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -34,6 +34,7 @@ export const createFolder = async () => {
   }
 }
 
+// eslint-disable-next-line max-len
 export const md5 = (s: string) => crypto.createHash('md5').update(s).digest('hex') // lgtm [js/weak-cryptographic-algorithm]
 
 export const time = () => +new Date()
